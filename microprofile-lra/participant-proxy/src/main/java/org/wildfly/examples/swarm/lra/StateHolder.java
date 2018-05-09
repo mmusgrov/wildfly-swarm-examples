a2a9085114b48d2c08b6851dcd2f75c42ede7afe
Michael Musgrove <michael.musgrove@gmail.com>
2018-05-09 18:36:52 +0100

    Fixes for bean scanning

---
+++ microprofile-lra/participant-proxy/src/main/java/org/wildfly/examples/swarm/lra/StateHolder.java
@@ -0,0 +1,27 @@
+package org.wildfly.examples.swarm.lra;
+
+import javax.inject.Singleton;
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Singleton
+public class StateHolder implements Serializable {
+    private AtomicInteger completedCount = new AtomicInteger(0);
+    private AtomicInteger compensatedCount = new AtomicInteger(0);
+
+    public int getCompletedCount() {
+        return completedCount.get();
+    }
+
+    public int incrCompletedCount() {
+        return completedCount.incrementAndGet();
+    }
+
+    public int getCompensatedCount() {
+        return compensatedCount.get();
+    }
+
+    public int incrCompensatedCount() {
+         return compensatedCount.incrementAndGet();
+    }
+}
